5788d2f738fd50a126d9ad1890e9aa9bdf313d0d
sowmya <sowmyachakravarthy6@gmail.com>
2025-07-03 18:31:52 +0530

    terraform vpc module

--- vpc.tf
+++ vpc.tf
@@ -148,7 +148,7 @@ resource "aws_route" "public" {
 }
 
 resource "aws_route" "private" {
-  route_table_id            = aws_route_table.public.id
+  route_table_id            = aws_route_table.private.id
   destination_cidr_block    = "0.0.0.0/0"
   gateway_id = aws_nat_gateway.main.id
 }
